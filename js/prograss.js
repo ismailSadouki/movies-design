58b0f9a705000a87bd1de6ca6a3909a818f332d6
ismail sadouki <khardalodzz@gmail.com>
2021-07-14 09:03:20 -0400

    first commit

---
+++ js/prograss.js
@@ -0,0 +1,10 @@
+
+
+let progress = document.getElementById('progressbar');
+
+let totalHeight = document.body.scrollHeight - window.innerHeight;
+
+window.onscroll = function () {
+  let progressHeight = (window.pageYOffset / totalHeight) * 100;
+  progress.style.height = progressHeight + "%";
+}
